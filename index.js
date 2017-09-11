7ac897d3521336e000037181cc8cf755324d6411
tynopet <dimangud@rambler.ru>
2017-09-11 11:36:43 +0300

    stateless kakoy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
-const https = require('https');
 const dateFns = require('date-fns');
+const Random = require('./prng');
 
 const kakoy = [
   'странный', 'зашкваренный', 'батутный', 'дерзкий',
@@ -15,23 +14,17 @@ const kakoy = [
   'твиксовый', 'местный', 'местный', 'текстайловый', 'нижний',
 ];
 
-let cache = {};
-let lastCleanCacheDate = new Date();
-
 const getKakoy = (userId) => {
   const currentDate = dateFns.startOfDay(new Date());
-  const cleanCacheDate = dateFns.startOfDay(dateFns.addDays(lastCleanCacheDate, 1));
-  if (dateFns.isBefore(cleanCacheDate, currentDate)) {
-    cache = {};
-    lastCleanCacheDate = new Date();
-  }
+  const seed = userId + dateFns.getTime(currentDate);
+  const random = new Random(seed);
+  const randomValue = random.next();
+  const wordsCount = (randomValue % 3) + 1;
 
-  if (cache[userId]) return cache[userId];
-  const parameters = Math.round(Math.random() * 3) + 2;
   let result = '';
   let tmp = [...kakoy];
   for (let i = 0; i < parameters; i++) {
-    const idx = Math.round(Math.random() * (tmp.length - 1));
+    const idx = new Random(seed + i).next() % kakoy.length;
     result += tmp[idx] + ' ';
     tmp = tmp.filter(el => el !== tmp[idx]);
   }
@@ -39,51 +32,23 @@ const getKakoy = (userId) => {
   return result;
 };
 
-const buildNotification = (msg, count) => {
-  const name = msg.from.username ? '@' + msg.from.username : msg.from.first_name;
-  const bingoCount = count > 1 ? ` x${count}` : '';
-  return `${name} выбивает бинго${bingoCount}`;
-};
+const token = process.env.TOKEN;
+const port = process.env.PORT || 8443;
+const bot = new TelegramBot(token, {
+  webHook: { port: port }
+});
 
-const createBot = (bingo) => {
-  const token = process.env.TOKEN;
-  const port = process.env.PORT || 8443;
-  const bot = new TelegramBot(token, {
-    webHook: { port: port }
-  });
+const url = process.env.HOST;
+bot.setWebHook(`${url}/bot${token}`);
 
-  const url = process.env.HOST;
-  console.log(url);
-  console.log(port);
-  bot.setWebHook(`${url}/bot${token}`);
+bot.onText(/\/stupeni/, function (msg) {
+  var chat_id = msg.chat.id;
+  var resp = '@tynopet @nastenkamurr @stop_kran @shellwiz @sinstarker @ustits @mazurikes ступени го';
+  bot.sendMessage(chat_id, resp);
+});
 
-  bot.onText(/\/stupeni/, function (msg) {
-    var chat_id = msg.chat.id;
-    var resp = '@Tynopet @nastenkamurr @stop_kran @shellwiz @sinstarker @ustits @mazurikes ступени го';
-    bot.sendMessage(chat_id, resp);
-  });
-
-  bot.onText(/\/kakoy/, (msg) => {
-    const chat_id = msg.chat.id;
-    const answer = getKakoy(msg.from.id);
-    bot.sendMessage(chat_id, `Сегодня ты ${answer} Онимовец!`);
-  });
-
-  bot.on('message', (message) => {
-    const chatId = message.chat.id;
-    if (message.text) {
-      const msg = message.text.toLowerCase();
-      bingoCount = bingo.reduce((acc, w) => msg.search(w) !== -1 ? acc + 1 : acc, 0)
-      if (bingoCount > 1) {
-        bot.sendMessage(chatId, '`Звуки бинго`');
-        bot.sendMessage(chatId, buildNotification(message, bingoCount));
-      }
-    }
-  });
-}
-
-fs.readFile(`${__dirname}/words.txt`, 'utf8', (err, data) => {
-  if (err) console.error(err);
-  const bingo = data.split(', ');
-  createBot(bingo);
+bot.onText(/\/kakoy/, (msg) => {
+  const chat_id = msg.chat.id;
+  const answer = getKakoy(msg.from.id);
+  bot.sendMessage(chat_id, `Сегодня ты ${answer} Онимовец!`);
 });
